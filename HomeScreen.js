b50e2f576f003b3541892a6bda233284f58ef9fa
rickliu131 <liuyuxuan131@outlook.com>
2021-10-29 01:13:38 -0400

    CPA3git commit

---
+++ HomeScreen.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Post from './DiaryView';
+
+function HomeScreen() {
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [count, setCount] = useState(-1);
+  const [content, setContent] = useState(<View></View>);
+
+  const updateCount = async(value) => {
+    await AsyncStorage.setItem('@count', value);
+  }
+
+  useEffect(() => {updateCount(count)}, [count]);
+
+  const getPosts = async() => {
+    let count = await AsyncStorage.getItem('@count');
+    if (count == null){
+      count = 0;
+    } else {
+      count = parseInt(count, 10);
+    }
+    setCount(count); //for 'clear' uses
+
+    let posts = [];
+    for (let i=count; i>=1; i--){
+      const key = 'post#' + i.toString();
+      let p = await AsyncStorage.getItem(key);
+      p = JSON.parse(p);
+      //extend this part later! (date, author, view...) (both at here and in DiaryView.js)
+      posts.push(<Post content={p.content} author={p.author_name} date={p.date}/>);
+      // alert(p.content);
+    }
+    setContent(<View style={styles.whole}>{posts}</View>);
+  }
+
+  const clearAll = async() => {
+    await AsyncStorage.clear();
+    setIsLoaded(false);
+  }
+
+  if (isLoaded == false){
+    getPosts();
+    setIsLoaded(true);
+  }
+
+  return(
+    <View style={{alignItems: 'center'}}>
+      <View style={{width: '100%'}}>{content}</View>
+      <View style={{marginTop:10, width: 150}}><Button title='Refresh' onPress={() => getPosts()} /></View>
+      <View style={{marginTop:10, width: 150}}><Button title='Clear' onPress={() => clearAll()} /></View>
+      <View style={{marginTop: 10}}><Text>You have {count} diary entries.</Text></View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  whole: {
+    marginTop: 30
+  }
+});
+
+
+export default HomeScreen;
