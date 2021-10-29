5fb21dd639739a80ba8a621400001ae88851047b
rickliu131 <liuyuxuan131@outlook.com>
2021-10-29 01:14:29 -0400

    cpa3!

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './HomeScreen';
+import WriteScreen from './WriteScreen';
+import AccountScreen from './AccountScreen';
+import SettingsScreen from './SettingsScreen';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+        <Tab.Screen name='Home' component={HomeScreen}
+                    options={{ tabBarIcon: ({ color, size }) => (<Ionicons name='journal' color={color} size={size} />) }} />
+        <Tab.Screen name='Write' component={WriteScreen}
+                    options={{ tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name='feather' color={color} size={size} />) }} />
+        <Tab.Screen name='Account' component={AccountScreen}
+                    options={{ tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name='account-heart' color={color} size={size} />) }} />
+        <Tab.Screen name='Settings' component={SettingsScreen}
+                    options={{ tabBarIcon: ({ color, size }) => (<Ionicons name='settings' color={color} size={size} />) }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
